3d0202edbae2b4d26f1efcab445502e5a8dbd92d
Tambu Jenny <tambujenny@gmail.com>
2022-09-12 04:50:41 +0100

    Welcome to NLW

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
+function Button() {
+  return (
+    <TouchableOpacity style ={styleButton.button}>
+      <Text style ={styleButton.text}>Hello React-Native</Text>
+    </TouchableOpacity>
+  );
+}
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Mc Tambu</Text>
+      <Button ></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  title: {
+    color: "white",
+    fontSize: 12
+  }
+});
+
+const styleButton = StyleSheet.create({
+
+  text:
+  {
+    color: "#F5EDDC",
+    fontSize:12
+  },
+
+  button:
+  {
+      backgroundColor: "#E94560",
+
+      margin: 25,
+      padding: 15,
+      borderRadius: 5,
+
+  }
+
+
 });
